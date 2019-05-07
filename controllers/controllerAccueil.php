8011175f0b297b492145a9b0bced14ca83ca79b9
Sep <at.septembre@gmail.com>
2019-05-07 16:24:29 +0200

    Copiés-collés, dernier commit

---
+++ controllers/controllerAccueil.php
@@ -0,0 +1,17 @@
+require_once 'modeles/recette.php';
+require_once 'framework/controller.php';
+require_once 'framework/vue.php';
+class ControllerAccueil extends Controller {
+	private $recette;
+	public function __construct() {
+		$this->recette = new Recette();
+	}
+	// Affiche la liste de toutes les recettes du blog
+	public function index() {
+		$recettes = $this->recette->getRecettes();
+		$this->genererVue(array('recette' => $recettes));
+	// code à implémenter
+	// récupérer la liste des recettes
+	// générer la vue
+	}
+}
---
+++ vues/accueil/index.php
@@ -0,0 +1,27 @@
+<div id="global">
+	<article>
+		<header>
+			<img class="imgRecette" src="img/tartiflette.jpg" width="300px"
+			height="242px" alt="Tartiflette" />
+			<a href="index.php?controller=recette&action=recette&id=id tartiflette">
+				<h1 class="titreRecette">
+					Tartiflette
+				</h1>
+			</a>
+			<time>
+				07/01/2019
+			</time>
+		</header>
+		<p>
+			La tartiflette savoyarde est un gratin de pommes de terre avec du Reblochon fondu dessus
+		</p>
+	</article>
+	<hr />
+</div> 
+
+<!-- ATTENTION : CETTE VUE DEVRA ÊTRE DYNAMIQUE, il faudra lui passer une liste de recettes dans une variable
+« recettes » et remplacer les informations statiques « id tartiflette », « Tartiflette »,
+« 07/01/2019 » et « La tartiflette savoyarde est un gratin de pommes de terre avec du
+Reblochon fondu dessus » par du code PHP correspondant aux valeurs de chaque recette contenu
+dans la liste. Utiliser pour parcourir la liste une boucle foreach. 
+-->
